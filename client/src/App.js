676744d641df4a0fe478939a18cfa21e42943f61
rls5113 <stuartrl@comcast.net>
2019-06-14 14:19:28 -0500

    fix styling on heroku?

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Search from "./pages/Search"
